878cbf839e248b1847015cdd88c3fcf9278aefff
Wither362 <93864752+Wither362@users.noreply.github.com>
2022-01-24 10:36:24 +0100

    Create Calcula.hx

---
+++ Calcula.hx
@@ -0,0 +1,22 @@
+import README
+import More
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.addons.transition.FlxTransitionableState;
+import flixel.effects.FlxFlicker;
+import flixel.graphics.frames.FlxAtlasFrames;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
+import flixel.math.FlxMath;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+import flixel.util.FlxColor;
+import lime.app.Application;
+import More;
+import editors.MasterEditorMenu;
+import flixel.input.keyboard.FlxKey;
+if More 'more'
+  bah
+ 
